15cc2c1865c02539d427259a348f5f9544969570
plan94322-rgbplaneam <plan94322@gmail.com>
2025-11-19 12:05:23 -0400

    Clean: remover node_modules y lock para Render

--- src/db.js
+++ src/db.js
@@ -105,6 +105,12 @@ function getLockedDatesForMonth(ym) {
   return rows.map((r) => r.date)
 }
 
+function resetApp() {
+  db.prepare('DELETE FROM counts').run()
+  db.prepare('DELETE FROM locks').run()
+  db.prepare('DELETE FROM users WHERE role != "admin"').run()
+}
+
 module.exports = {
   init,
   seedAreasUnits,
@@ -123,4 +129,5 @@ module.exports = {
   unlockDate,
   isDateLocked,
   getLockedDatesForMonth,
+  resetApp,
 }
